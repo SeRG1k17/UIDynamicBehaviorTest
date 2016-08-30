54762eae6e929669ef5dbdb41c82cc0ee12368b2
SeRG1k17 <sergei.pygach@gmail.com>
2016-08-30 23:01:04 +0300

    Initial Commit

---
+++ UIDynamicBehaviorTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIDynamicBehaviorTest
+//
+//  Created by Сергей Пугач on 30.08.16.
+//  Copyright © 2016 Сергей Пугач. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
